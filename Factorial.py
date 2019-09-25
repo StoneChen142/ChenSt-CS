b2fda05e81fb9e20ce21aee310904cfa7586512c
StoneChen142 <ChenSt@dulwich.org.uk>
2019-09-25 18:46:34 +0100

    Factorial
    Updated

--- Factorial.py
+++ Factorial.py
@@ -1,17 +1,28 @@
-def factorial(n):
+def factorial(n,result):
+   n=int(input("Enter a number: "))
    o=1
-   for i in range(1,n+1):
-         o*=i
-         i+=2
-   print(o)
+   if n>0:
+      for i in range(1,n+1):
+            o*=i
+            i+=2
+      result=o
+      n=n
+      print(n,result)
+   elif n==0:
+      print("1")
+   else:
+      print("Please enter a number greater or equal to 0")
+      factorial(n,result)
+   
+def again():
+   a=bool(input("Again? 1=Yes, 0=No : "))
+   if a==True:
+      factorial(n,result)
+      again()
 
-n=int(input())
+result=0
+n=0
 
-if n==0:
-   print("1")
-elif n>0:
-   factorial(n)
-else:
-   print("Please enter a number greater or equal to 0")
-   n=int(input())
-   factorial(n)
+factorial(n,result)
+
+again()

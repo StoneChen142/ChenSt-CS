e07c9358e67e364b0e73fe7f7d617e1118668774
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2019-09-29 18:14:10 +0100

    Feedback: Rock paper scissors
    This is a good effort and I particularly liked the multiple rounds and keeping the score. Read the comments in the program and update please.

--- RPS.py
+++ RPS.py
@@ -2,14 +2,14 @@
 from random import randint
 
 round = 1
-round = int(round)
-y = 0
+round = int(round) # SRC - What is the purpose of this line?
+y = 0 ### SRC - Please use meaningful variable name such as: computer_score
 x = 0
 
 while round < 7:
     pc = int(input("Round ("+str(round)+"/6) Rock(1),Paper(2),Scissors(3): "))
     cc = int(randint(1,3))
-    x= int(x)
+    x= int(x)  ### SRC - Againg what is the point of this? I think there may be a misunderstanding here.
     y = int(y)
     round+=1
 
@@ -32,11 +32,13 @@ while round < 7:
         x+=1
         print("You obtained a point! Computer used Paper  "+"Player: "+str(x)+"  Computer: "+str(y))
     elif pc==1 and cc==1:
-        print("Draw!  "+"Player: "+str(x)+"  Computer: "+str(y))
+        print("Draw!  "+"Player: "+str(x)+"  Computer: "+str(y))   ### SRC - Could you make these last three if statements just one?
     elif pc==2 and cc==2:
         print("Draw!  "+"Player: "+str(x)+"  Computer: "+str(y))
     elif pc==3 and cc==3:
         print("Draw!  "+"Player: "+str(x)+"  Computer: "+str(y))
+
+### SRC - Please use end while comment.
 
 if x>y:
     print("You Win!")

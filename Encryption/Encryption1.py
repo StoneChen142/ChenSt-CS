99265f8ed7c538a395cf54f5d16ed39a9497de01
StoneChen142 <ChenSt@dulwich.org.uk>
2019-10-03 20:20:38 +0100

    Update Encryption1.py
    Encrypting&Decrypting text completed

--- Encryption/Encryption1.py
+++ Encryption/Encryption1.py
@@ -8,18 +8,37 @@ f.close()
 #Encrypt
 
 encrypted_text=""
-i=1
+i=0
 while i<length:
     word=data[i]
     if word.islower():
-        encrypted_text = encrypted_text + str(word.upper)
-    elif word.islower():
-        encrypted_text = encrypted_text + str(word.lower)
+        encrypted_text = encrypted_text + chr(ord(word.upper())+3)
+    elif word.isupper():
+        encrypted_text = encrypted_text + chr(ord(word.lower())+3)
+    else:
+        encrypted_text = encrypted_text + chr(ord(word)+3)
     i+=1
     #endif
 #endwhile
 
 print("Encrypted text: ",encrypted_text)
+print("")
+
 #Decrypt
+decrypted_text=""
+i=0
+while i<length:
+    word=encrypted_text[i]
+    if word.islower():
+        decrypted_text = decrypted_text + chr(ord(word.upper())-3)
+    elif word.isupper():
+        decrypted_text = decrypted_text + chr(ord(word.lower())-3)
+    else:
+        decrypted_text = decrypted_text + chr(ord(word)-3)
+    i+=1
+    #endif
+#endwhile
+
+print("Decrypted text: ",decrypted_text)
 #ord()
 #chr()

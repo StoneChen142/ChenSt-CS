e9684d53a02174ea5a82ca9cb814b2b79abf1ce0
StoneChen142 <ChenSt@dulwich.org.uk>
2019-11-12 13:11:36 +0000

    Sprite

--- myFirstClass.py
+++ myFirstClass.py
@@ -18,24 +18,57 @@ DGREEN = (0,51,0)
 LY = (255,255,204)
 NICE = (204,204,255)
 # -- Classes
+#Super class
+#class WoodenBall(Ball):
+    #def __init__(self, colour):
 
+#Sub class
 class Ball():
     def __init__(self, colour):
-        self.x = randint(10,590)
-        self.y = randint(10,590)
+        self.x = randint(20,580)
+        self.y = randint(20,580)
+        self.region = randint(1,4)
         self.speedx = randint(2,4)
         self.speedy = randint(2,4)
         self.colour = colour
 
     def move(self):
-        if self.y == 590 or self.y > 590:
-            self.speedy = self.speedy*-1
-        elif self.y == 10 or self.y < 10:
-            self.speedy = self.speedy*-1
-        elif self.x == 10 or self.x < 10:
-            self.speedx = self.speedx*-1
-        elif self.x == 590 or self.x > 590:
-            self.speedx = self.speedx*-1
+        if self.region == 1:
+            if self.y == 290 or self.y > 290:
+                self.speedy = self.speedy*-1
+            elif self.y == 10 or self.y < 10:
+                self.speedy = self.speedy*-1
+            elif self.x == 10 or self.x < 10:
+                self.speedx = self.speedx*-1
+            elif self.x == 290 or self.x > 290:
+                self.speedx = self.speedx*-1
+        if self.region == 2:
+            if self.y == 290 or self.y > 290:
+                self.speedy = self.speedy*-1
+            elif self.y == 10 or self.y < 10:
+                self.speedy = self.speedy*-1
+            elif self.x == 310 or self.x < 310:
+                self.speedx = self.speedx*-1
+            elif self.x == 590 or self.x > 590:
+                self.speedx = self.speedx*-1
+        if self.region == 3:
+            if self.y == 590 or self.y > 590:
+                self.speedy = self.speedy*-1
+            elif self.y == 310 or self.y < 310:
+                self.speedy = self.speedy*-1
+            elif self.x == 10 or self.x < 10:
+                self.speedx = self.speedx*-1
+            elif self.x == 290 or self.x > 290:
+                self.speedx = self.speedx*-1
+        if self.region == 4:
+            if self.y == 590 or self.y > 590:
+                self.speedy = self.speedy*-1
+            elif self.y == 310 or self.y < 310:
+                self.speedy = self.speedy*-1
+            elif self.x == 310 or self.x < 310:
+                self.speedx = self.speedx*-1
+            elif self.x == 590 or self.x > 590:
+                self.speedx = self.speedx*-1
 
         self.x += self.speedx
         self.y += self.speedy
@@ -60,10 +93,14 @@ pygame.display.set_caption("My First Flipbook")
 game_over = False
 
 ball_list = []
-for n in range(50):
+for n in range(5):
     my_ball = Ball(NICE)
     ball_list.append(my_ball)
-
+    my_ball = Ball(CYAN)
+    ball_list.append(my_ball)
+#endfor
+    
+    
 ### -- Game Loop
 while not game_over:
     # -- User input and controls
@@ -75,9 +112,10 @@ while not game_over:
 
     screen.fill(BLACK)
 
-    for n in range(50):
+    for n in range(10):
         ball_list[n].move()
         ball_list[n].draw()
+    #endfor
     
     # -- flip display to reveal new position of objects
     pygame.display.flip()
